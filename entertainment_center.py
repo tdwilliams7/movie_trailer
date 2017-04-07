30ca2ca93be4211ef45e8b791417797af1601819
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-04-07 12:03:19 -0500

    add storyline to master chage html under movie tiles to display storyline <h3>{storyline}</h3>

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,35 +5,35 @@ import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "a story of a boy and his toys that come to life",
-                        "http://www.impawards.com/1995/posters/toy_story_ver1.jpg",
+                        'http://www.impawards.com/1995/posters/toy_story_ver1.jpg',
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
 
 avatar = media.Movie("Avatar",
                      "A marine on an Alien Planet.",
-                     "https://www.movieposter.com/posters/archive/main/98/MPW-49433",
+                     'https://www.movieposter.com/posters/archive/main/98/MPW-49433',
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
 
 guardians = media.Movie("Guardians of the Galaxy",
                         "A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.",
-                        "http://www.mtv.com/crop-images/2014/02/20/Guardians-of-the-Galaxy-Poster.jpg",
+                        'http://www.mtv.com/crop-images/2014/02/20/Guardians-of-the-Galaxy-Poster.jpg',
                         "https://www.youtube.com/watch?v=maIEaTm5gBE")
 #guardians.Show_trailer()
 
 colossal = media.Movie ("Colossal",
                         "Anne Hathaway plays a burned-out party girl navigating life back in her hometown…just as a giant monster appears in South Korea mimicking her movements thousands of miles away.",
-                        "http://cdn2-www.comingsoon.net/assets/uploads/gallery/colossal/colossal-poster2.jpg",
+                        'http://cdn2-www.comingsoon.net/assets/uploads/gallery/colossal/colossal-poster2.jpg',
                         "https://www.youtube.com/watch?v=Fmk3cTuGQZI")
 
 rogue = media.Movie ("Rogue One",
                       "The Rebel Alliance makes a risky move to steal the plans for the Death Star, setting up the epic saga to follow.",
-                      "http://t0.gstatic.com/images?q=tbn:ANd9GcQ0S5JQhVplHbw7O6nt7Q0r23Bssl9UNzC-z3zy1r45_eLUB43l",
+                      'http://t0.gstatic.com/images?q=tbn:ANd9GcQ0S5JQhVplHbw7O6nt7Q0r23Bssl9UNzC-z3zy1r45_eLUB43l',
                       "https://www.youtube.com/watch?v=sC9abcLLQpI")
 
 kong = media.Movie ("Kong Skull Island",
                     "A team of scientists explore an uncharted island in the Pacific, venturing into the domain of the mighty Kong, and must fight to escape a primal Eden.",
-                    "http://t3.gstatic.com/images?q=tbn:ANd9GcS3OOr06uE3lEZwum5WxKkkdsC37ObLwjKZgSSx-V96yrt6DhKE",
+                    'http://t3.gstatic.com/images?q=tbn:ANd9GcS3OOr06uE3lEZwum5WxKkkdsC37ObLwjKZgSSx-V96yrt6DhKE',
                     "https://www.youtube.com/watch?v=AP0-9FBs2Rs")
                     
 
--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -122,6 +122,9 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <h3>{movie_storyline}</h3>
+    
+    
 </div>
 '''
 
@@ -138,7 +141,11 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            movie_storyline=movie.storyline
+
+
+
         )
     return content
 
